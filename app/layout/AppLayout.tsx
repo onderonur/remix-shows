1eac3802ab1decbdac992b55459cdc2e8e0dc5ce
onderonur <onuronder.onr@gmail.com>
2022-12-19 19:01:05 +0300

    app layout fix

--- app/layout/AppLayout.tsx
+++ app/layout/AppLayout.tsx
@@ -1,15 +1,10 @@
 import { Box, Container, Grid, GridItem } from '@chakra-ui/react';
 import { useCallback, useState } from 'react';
-import { APP_TITLE } from '~/common/CommonUtils';
 import AppTitle from '~/layout/AppTitle';
 import AppHeader from './AppHeader';
 import AppDrawer from './AppDrawer';
 import AppNavigation from './AppNavigation';
-import {
-  APP_DRAWER_WIDTH,
-  APP_FOOTER_HEIGHT,
-  APP_HEADER_HEIGHT,
-} from './LayoutUtils';
+import { APP_DRAWER_WIDTH, APP_HEADER_HEIGHT } from './LayoutUtils';
 
 type AppLayoutProps = React.PropsWithChildren<{}>;
 
@@ -30,9 +25,8 @@ export default function AppLayout({ children }: AppLayoutProps) {
         gridTemplateAreas={`
         'appHeader appHeader'
         'appSideBar appContent'
-        'appSideBar appFooter'
         `}
-        gridTemplateRows={`${APP_HEADER_HEIGHT} 1fr ${APP_FOOTER_HEIGHT}`}
+        gridTemplateRows={`${APP_HEADER_HEIGHT} 1fr`}
         gridTemplateColumns={{ base: '0 1fr', lg: `${APP_DRAWER_WIDTH} 1fr` }}
       >
         <GridItem gridArea="appHeader">
@@ -59,17 +53,6 @@ export default function AppLayout({ children }: AppLayoutProps) {
           >
             {children}
           </Container>
-        </GridItem>
-        <GridItem gridArea="appFooter">
-          <Box
-            height={APP_FOOTER_HEIGHT}
-            background="chakra-subtle-bg"
-            alignItems="center"
-            padding={4}
-            as="footer"
-          >
-            {APP_TITLE} &copy; {new Date().getFullYear()}
-          </Box>
         </GridItem>
       </Grid>
       <AppDrawer
--- app/layout/LayoutUtils.tsx
+++ app/layout/LayoutUtils.tsx
@@ -1,3 +1,2 @@
 export const APP_HEADER_HEIGHT = '70px';
-export const APP_FOOTER_HEIGHT = '64px';
 export const APP_DRAWER_WIDTH = '224px';
