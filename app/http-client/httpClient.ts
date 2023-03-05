d824e339369376155da0e001256abce0466282b8
onderonur <onuronder.onr@gmail.com>
2023-03-05 22:55:32 +0300

    minor refactor

--- app/http-client/httpClient.ts
+++ app/http-client/httpClient.ts
@@ -5,21 +5,15 @@ export const httpClient = {
     endpoint: string,
     params?: Record<string, string | number | undefined | null>,
   ): Promise<T> => {
-    const searchParams = new URLSearchParams({
-      api_key: process.env.API_KEY as string,
-    });
+    const url = new URL(`/3${endpoint}`, 'https://api.themoviedb.org');
+
+    url.searchParams.set('api_key', process.env.API_KEY as string);
 
     Object.entries(params ?? {}).forEach(([key, value]) => {
       if (value !== null && value !== undefined) {
-        searchParams.set(key, value.toString());
+        url.searchParams.set(key, value.toString());
       }
     });
-
-    let url = `https://api.themoviedb.org/3${endpoint}`;
-    const queryString = searchParams.toString();
-    if (queryString) {
-      url = `${url}?${queryString}`;
-    }
 
     const response = await fetch(url);
 
